546c984fc639cb3a5ea259ebc1c05765247f2aad
shazrin <wldrnz@gmail.com>
2018-11-26 15:25:58 +0800

    Scenes should manage updating and states of entities

--- src/game/scene/scene.cpp
+++ src/game/scene/scene.cpp
@@ -4,22 +4,4 @@ Scene::Scene()
 { }
 
 Scene::~Scene()
-{
-	for (auto entity : entities) 
-	{
-		delete entity;
-	}
-}
-
-void Scene::Update(float deltaTime)
-{
-	for (auto entity : entities)
-	{
-		entity->Update(deltaTime);
-	}
-}
-
-void Scene::AddGameEntity(GameEntity *entity)
-{
-	entities.push_back(entity);
-}
+{ }
--- src/game/scene/scene.h
+++ src/game/scene/scene.h
@@ -14,15 +14,11 @@ enum SceneType
 
 class Scene
 {
-private:
-	std::vector<GameEntity *> entities;
-
-	void AddGameEntity(GameEntity *entity);
 public:
 	Scene();
 	virtual ~Scene();
 
 	virtual void Begin() = 0;
-	virtual void Update(float deltaTime);
+	virtual void Update(float deltaTime) = 0;
 	virtual void End() = 0;
 };
