e6ea344f3901f630e23ef59cce69b213b79ad55e
shazrin <wldrnz@gmail.com>
2018-11-27 23:09:30 +0800

    Added hud

---
+++ assets/map/hud_box.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="hud_box" tilewidth="16" tileheight="16" tilecount="9" columns="3">
+ <image source="hud_box.png" width="48" height="48"/>
+</tileset>
